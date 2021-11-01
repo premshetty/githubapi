d36e64f3ad4d14c883b54423e5fd34511663d840
prem <premenaga@gmail.com>
2021-11-01 15:03:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import UserTable from './components/Table'
+import Prem from './components/Prem'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Router>
+
+     
+          <Switch>
+        <Route path='/prem'  component={Prem} />
+        <Route path='/' component={UserTable} />
+       </Switch>
+      </Router>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
